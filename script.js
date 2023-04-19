8cde0f79b46ad4a845b4dffb895d19db57ee8720
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-04-19 11:34:12 -0300

    First animation

---
+++ script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+
+let timer = null;
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    
+    ctx.drawImage(playerImage,
+        frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight,
+        0, 0, spriteWidth, spriteHeight);
+    
+    if (frameX < 6) frameX ++;
+    else frameX = 0;
+    
+    cancelAnimationFrame(timer);
+    timer = requestAnimationFrame(animate);
+};
+
+animate();
